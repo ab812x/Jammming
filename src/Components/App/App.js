90ad8f87a155555c4ec819067b19b7e9667811ce
ab812x <abdala.aa51511@gmail.com>
2024-09-13 01:02:53 +0300

    second commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.css";
+import SearchResults from "../SearchResults/SearchResults";
 function App() {
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: "examble track name 1",
+      artist: "examble track artist 1",
+      album: "example track album 1",
+      id: 1,
+    },
+    {
+      name: "examble track name 2",
+      artist: "examble track artist 2",
+      album: "example track album 2",
+      id: 2,
+    },
+  ]);
+
   return (
     <div>
       <h1>
-        Ja<span className="highlight">mmm</span>ing
+        Ja<span className={styles.highlight}>mmm</span>ing
       </h1>
-      <div className="App">
+      <div className={styles.App}>
         {/* <!-- Add a SearchBar component --> */}
 
-        <div className="App-playlist">
+        <div className={styles["App-playlist"]}>
           {/* <!-- Add a SearchResults component --> */}
+          <SearchResults userSearchResults={searchResults} />
           {/* <!-- Add a Playlist component --> */}
         </div>
       </div>
